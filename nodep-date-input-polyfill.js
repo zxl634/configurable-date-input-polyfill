7defc61ccd4483b321b7d21aa9b69e752e73a175
Brian Blakely <anewpage.media@gmail.com>
2016-03-08 04:22:01 +0000

    Full 1.0 features and backend.

--- nodep-date-input-polyfill.js
+++ nodep-date-input-polyfill.js
@@ -1,283 +1,355 @@
 import './nodep-date-input-polyfill.scss';
 
 class Picker {
-  constructor(input) {
-    this.theInput = input;
-    this.thePicker = null;
-    this.pickerDate = new Date();
-    this.pickerDate.setHours(0,0,0,0);
-    
-    this.inputDate = new Date(this.pickerDate.getTime());
-
-    this.getDateFromInput();
-    this.createPicker();
-  }
-
-  // Update selectedDate with the date from the input, return true if changed.
-  getDateFromInput() {
-    if(!this.theInput.value) {
-      return false;
+  constructor() {
+    // This is a singleton.
+    if(thePicker) {
+      return thePicker;
     }
-    
-    const possibleNewDate = new Date(this.theInput.value.replace(/-/g, `/`));
-    if(
-      Date.parse(this.theInput.value)
-      && possibleNewDate.toDateString() !== this.pickerDate.toDateString()
-    ) {
-      this.pickerDate = possibleNewDate;
-      this.inputDate = new Date(possibleNewDate.getTime());
-      return true;
-    }
-  }
 
-  // Create the picker html and events.
-  createPicker() {
-    // The picker div.
-    this.thePicker = document.createElement(`date-input-polyfill`);
-    this.thePicker.style.display = `none`;
-    document.body.appendChild(this.thePicker);
+    this.date = new Date();
+    this.input = null;
+    this.isOpen = false;
 
-    // The year and month selects inside the picker.
-    this.createYearAndMonthSelects();
+    // The picker element. Unique tag name attempts to protect against
+    // generic selectors.
+    this.container = document.createElement(`date-input-polyfill`);
 
-    // The days table inside the picker.
-    this.createMonthTable();
-
-    // Open the picker when the input get focus,
-    // also on various click events to capture it in all corner cases.
-    const showPicker = ()=> {
-      const rekt = this.theInput.getBoundingClientRect();
-      this.thePicker.style.top = `${
-        rekt.top + rekt.height
-        + document.documentElement.scrollTop + document.body.scrollTop
-      }px`;
-      this.thePicker.style.left = `${
-        rekt.left
-        + document.documentElement.scrollLeft + document.body.scrollLeft
-      }px`;
-      this.thePicker.style.display = `block`;
-    };
-    this.theInput.addEventListener(`focus`, showPicker);
-    this.theInput.addEventListener(`mouseup`, showPicker);
-    this.theInput.addEventListener(`mousedown`, showPicker);
-
-    // Update the picker if the date changed manually in the input.
-    this.theInput.addEventListener(`keyup`, ()=> {
-      if(this.getDateFromInput()) {
-        this.updateSelects();
-      }
-    });
-
-    // Close the picker when clicking outside of the input or picker.
-    document.addEventListener(`click`, e=> {
-      if(
-        e.target !== this.theInput
-        && e.target !== this.thePicker
-        && e.target.parentNode !== this.thePicker
-        && e.target.parentNode.parentNode !== this.thePicker
-      ) {
-        this.thePicker.style.display = `none`;
-      }
-    });
-  }
-
-  // Create the year and month selects html.
-  createYearAndMonthSelects() {
-    // The year selector inside the picker.
-    const yearSelect = this.createRangeSelect(
-      new Date().getFullYear() - 80,
-      new Date().getFullYear() + 20,
-      this.pickerDate.getFullYear()
+    // Add controls.
+    // Year picker.
+    this.year = Picker.createRangeSelect(
+      this.date.getFullYear() - 80,
+      this.date.getFullYear() + 20
     );
-    yearSelect.className = `yearSelect`;
-    this.thePicker.appendChild(yearSelect);
-    yearSelect.addEventListener(`change`, ()=> {
-      this.pickerDate.setYear(yearSelect.value);
-      this.createMonthTable();
-      this.theInput.focus();
+    this.year.className = `yearSelect`;
+    this.year.addEventListener(`change`, ()=> {
+      this.date.setYear(this.year.value);
+      this.refreshDaysMatrix();
     });
+    this.container.appendChild(this.year);
 
-    // The month selector inside the picker.
-    const monthsNames = [
-      `January`, 
-      `February`, 
-      `March`, 
-      `April`, 
-      `May`, 
-      `June`, 
-      `July`, 
-      `August`, 
-      `September`, 
-      `October`, 
-      `November`, 
+    // Month picker.
+    const monthLabels = [
+      `January`,
+      `February`,
+      `March`,
+      `April`,
+      `May`,
+      `June`,
+      `July`,
+      `August`,
+      `September`,
+      `October`,
+      `November`,
       `December`
     ];
-    const monthSelect = this.createRangeSelect(0, 11, this.pickerDate.getMonth(), monthsNames);
-    monthSelect.className = `monthSelect`;
-    this.thePicker.appendChild(monthSelect);
-    monthSelect.addEventListener(`change`, ()=> {
-      this.pickerDate.setMonth(monthSelect.value);
-      this.createMonthTable();
-      this.theInput.focus();
+    this.month = Picker.createRangeSelect(
+      0,
+      11,
+      monthLabels
+    );
+    this.month.className = `monthSelect`;
+    this.month.addEventListener(`change`, ()=> {
+      this.date.setMonth(this.month.value);
+      this.refreshDaysMatrix();
+    });
+    this.container.appendChild(this.month);
+
+    // Today button.
+    this.today = document.createElement(`button`);
+    this.today.textContent = `Today`;
+    this.today.addEventListener(`click`, ()=> {
+      this.date = new Date();
+      this.setInput();
+    });
+    this.container.appendChild(this.today);
+
+    // Setup unchanging DOM for days matrix.
+    const daysMatrix = document.createElement(`table`);
+    const daysHead = document.createElement(`thead`);
+    daysHead.innerHTML = `
+      <thead>
+        <tr>
+          <th scope="col">Sun</th>
+          <th scope="col">Mon</th>
+          <th scope="col">Tue</th>
+          <th scope="col">Wed</th>
+          <th scope="col">Thu</th>
+          <th scope="col">Fri</th>
+          <th scope="col">Sat</th>
+        </tr>
+      </thead>
+    `;
+
+    this.days = document.createElement(`tbody`);
+
+    // THIS IS THE BIG PART.
+    // When the user clicks a day, set that day as the date.
+    // Uses event delegation.
+    this.days.addEventListener(`click`, e=> {
+      const tgt = e.target;
+
+      if(!tgt.hasAttribute(`data-day`)) {
+        return false;
+      }
+
+      const curSel = this.days.querySelector(`[data-selected]`);
+      if(curSel) {
+        curSel.removeAttribute(`data-selected`);
+      }
+      tgt.setAttribute(`data-selected`, ``);
+
+      this.date.setDate(parseInt(tgt.textContent));
+      this.setInput();
+    });
+
+    daysMatrix.appendChild(daysHead);
+    daysMatrix.appendChild(this.days);
+    this.container.appendChild(daysMatrix);
+
+    this.hide();
+    document.body.appendChild(this.container);
+
+    // Close the picker when clicking outside of a date input or picker.
+    document.addEventListener(`click`, e=> {
+      let el = e.target;
+      let isPicker = el === this.container;
+
+      while(!isPicker && (el = el.parentNode)) {
+        isPicker = el === this.container;
+      }
+
+      e.target.getAttribute(`type`) !== `date` && !isPicker
+        && this.hide();
     });
   }
 
-  // Update the year and month selects with the right selected value (if date changed externally).
-  updateSelects() {
-    this.thePicker.querySelector(`.yearSelect`).value  = this.pickerDate.getFullYear();
-    this.thePicker.querySelector(`.monthSelect`).value = this.pickerDate.getMonth();
-    this.createMonthTable();
+  // Hide.
+  hide() {
+    this.container.setAttribute(`data-open`, this.isOpen = false);
   }
 
-  // Create the days table.
-  createMonthTable() {
-    const year = this.pickerDate.getFullYear(); // Get the year (2015).
-    const month = this.pickerDate.getMonth(); // Get the month number (0-11).
+  // Show.
+  show() {
+    this.container.setAttribute(`data-open`, this.isOpen = true);
+  }
+
+  // Position picker below element. Align to element's left edge.
+  goto(element) {
+    const rekt = element.getBoundingClientRect();
+    this.container.style.top = `${
+      rekt.top + rekt.height
+      + document.documentElement.scrollTop + document.body.scrollTop
+    }px`;
+    this.container.style.left = `${
+      rekt.left
+      + document.documentElement.scrollLeft + document.body.scrollLeft
+    }px`;
+
+    this.show();
+  }
+
+  // Initiate I/O with given date input.
+  attachTo(input) {
+    if(
+      input === this.input
+      && this.isOpen
+    ) {
+      return false;
+    }
+
+    this.input = input;
+    this.sync();
+    this.goto(this.input);
+  }
+
+  // Match picker date with input date.
+  sync() {
+    if(this.input.valueAsDate) {
+      this.date = this.input.valueAsDate;
+    }
+
+    this.year.value = this.date.getFullYear();
+    this.month.value = this.date.getMonth();
+    this.refreshDaysMatrix();
+  }
+
+  // Match input date with picker date.
+  setInput() {
+    this.input.valueAsDate = this.date;
+    this.input.focus();
+    this.hide();
+
+    // Make AngularJS see the change.
+    const fakeEvent = document.createEvent(`KeyboardEvent`);
+    fakeEvent.initEvent(`change`, true, false);
+    this.input.dispatchEvent(fakeEvent);
+  }
+
+  refreshDaysMatrix() {
+    // Determine days for this month and year,
+    // as well as on which weekdays they lie.
+    const year = this.date.getFullYear(); // Get the year (2016).
+    const month = this.date.getMonth(); // Get the month number (0-11).
     const startDay = new Date(year, month, 1).getDay(); // First weekday of month (0-6).
     const maxDays = new Date(
-      this.pickerDate.getFullYear(), 
-      month + 1, 
+      this.date.getFullYear(),
+      month + 1,
       0
     ).getDate(); // Get days in month (1-31).
 
-    // If there was a table before, remove it.
-    const oldTables = this.thePicker.getElementsByTagName(`table`);
-    if(oldTables.length > 0) {
-      this.thePicker.removeChild(oldTables[0]);
-    }
+    // The input's current date.
+    const selDate = this.input.valueAsDate || this.date;
 
-    // The table and header forthe month days.
-    const theTable = document.createElement(`table`);
-    theTable.innerHTML = `
-      <tr>
-        <th>Sun</th>
-        <th>Mon</th>
-        <th>Tue</th>
-        <th>Wed</th>
-        <th>Thu</th>
-        <th>Fri</th>
-        <th>Sat</th>
-      </tr>
-    `;
-    this.thePicker.appendChild(theTable);
+    // Are we in the input's currently-selected month and year?
+    // Or just browsing?
+    const selMatrix =
+      year === selDate.getFullYear()
+      && month === selDate.getMonth();
 
-    // Create the days cols according to the selected month days.
-    let aRow;
-    const aDate = new Date(this.pickerDate.getTime());
-    for(let cellNum = 0; cellNum < maxDays + startDay; cellNum++) {
-      // Create a table row in the begining and after each 7 cells.
-      if(cellNum % 7 === 0) {
-        aRow = theTable.insertRow(-1);
+    // Populate days matrix.
+    let matrixHTML = [];
+    for(let i = 0; i < maxDays + startDay; ++i) {
+      // Add a row every 7 days.
+      if(i % 7 === 0) {
+        matrixHTML.push(`
+          ${i !== 0 ? `</tr>` : ``}
+          <tr>
+        `);
       }
 
-      const aCell = aRow.insertCell(-1);
-
-      if(cellNum + 1 > startDay) {
-
-        const dayNum = cellNum + 1 - startDay;
-        aCell.innerHTML = dayNum;
-        
-        aDate.setDate(dayNum);
-        if(aDate.getTime() === this.inputDate.getTime()) {
-          aCell.className = `selected`;
-        }
-
-        // When clicking on a day in the days table.
-        aCell.addEventListener(`click`, ()=> {
-          // Mark the dey with `selected` css class.
-          const curSel = this.thePicker.querySelector(`.selected`);
-          if(curSel) {
-            curSel.className = ``;
-          }
-          aCell.className = `selected`;
-
-          this.pickerDate.setDate(parseInt(aCell.innerHTML));
-          this.selectDate();
-          this.theInput.focus();
-        });
+      // Add new column.
+      // If no days from this month in this column, it will be empty.
+      if(i + 1 <= startDay) {
+        matrixHTML.push(`<td></td>`);
+        continue;
       }
+
+      // Populate day number.
+      const dayNum = i + 1 - startDay;
+      const selected = selMatrix && selDate.getDate() === dayNum;
+
+      matrixHTML.push(
+        `<td data-day ${selected ? `data-selected` : ``}>
+          ${dayNum}
+        </td>`
+      );
     }
+
+    this.days.innerHTML = matrixHTML.join(``);
   }
 
-  // Copy the selected date to the input field.
-  selectDate() {
-    let monthText = this.pickerDate.getMonth() + 1;
-    if(monthText < 10) {
-      monthText = `0` + monthText;
-    }
-
-    let dayText = this.pickerDate.getDate();
-    if(dayText < 10) {
-      dayText = `0` + dayText;
-    }
-
-    this.inputDate = new Date(this.pickerDate.getTime());
-    this.theInput.value = `${this.pickerDate.getFullYear()}-${monthText}-${dayText}`;
-
-    // Make angular see the change.
-    const fakeEvent = document.createEvent(`KeyboardEvent`);
-    fakeEvent.initEvent(`change`, true, false);
-    this.theInput.dispatchEvent(fakeEvent);
-  }
-
-  // Helper function to create html select tags.
-  createRangeSelect(min, max, selected, namesArray) {
+  static createRangeSelect(min, max, namesArray) {
     const theSelect = document.createElement(`select`);
 
-    for(let curNum = min; curNum <= max; curNum++) {
-      let theText;
-      
+    for(let i = min; i <= max; ++i) {
       const aOption = document.createElement(`option`);
       theSelect.appendChild(aOption);
 
-      if(namesArray) {
-        theText = namesArray[curNum - min];
-      } else {
-        theText = curNum;
-      }
+      const theText = namesArray ? namesArray[i - min] : i;
 
       aOption.text = theText;
-      aOption.value = curNum;
-
-      if(curNum === selected) {
-        aOption.selected = true;
-      }
+      aOption.value = i;
     };
 
     return theSelect;
   }
-  
+}
+
+const thePicker = new Picker();
+
+class Input {
+  constructor(input) {
+    this.element = input;
+    this.element.setAttribute(`data-has-picker`, ``);
+
+    Object.defineProperties(
+      this.element,
+      {
+        'valueAsDate': {
+          get: ()=> {
+            if(!this.element.value) {
+              return null;
+            }
+
+            const val = this.element.value.split(/\D/);
+            return new Date(val[0], --val[1], val[2]);
+          },
+          set: val=> {
+            const day = `0${val.getDate()}`.slice(-2),
+                month = `0${val.getMonth() + 1}`.slice(-2),
+                datestr = `${val.getFullYear()}-${month}-${day}`;
+
+            this.element.value = datestr;
+          }
+        },
+        'valueAsNumber': {
+          get: ()=> {
+            if(!this.element.value) {
+              return NaN;
+            }
+
+            const val = this.element.value.split(/\D/);
+            return new Date(val[0], --val[1], val[2]).getTime();
+          },
+          set: val=> {
+            const date = new Date(val),
+                day = `0${date.getDate()}`.slice(-2),
+                month = `0${date.getMonth() + 1}`.slice(-2),
+                datestr = `${date.getFullYear()}-${month}-${day}`;
+
+            this.element.value = datestr;
+          }
+        }
+      }
+    );
+
+    // Open the picker when the input get focus,
+    // also on various click events to capture it in all corner cases.
+    const showPicker = ()=> {
+      thePicker.attachTo(this.element);
+    };
+    this.element.addEventListener(`focus`, showPicker);
+    this.element.addEventListener(`mousedown`, showPicker);
+    this.element.addEventListener(`mouseup`, showPicker);
+
+    // Update the picker if the date changed manually in the input.
+    this.element.addEventListener(`keyup`, ()=> {
+      thePicker.sync();
+    });
+  }
+
   // Return false if the browser does not support input[type="date"].
-  static checkDateInputSupport() {
+  static supportsDateInput() {
     const input = document.createElement(`input`);
     input.setAttribute(`type`, `date`);
-  
+
     const notADateValue = `not-a-date`;
     input.setAttribute(`value`, notADateValue);
-  
+
     return !(input.value === notADateValue);
   }
-  
+
   // Will add the Picker to all inputs in the page.
   static addPickerToDateInputs() {
-    // Get and loop all the input[type="date"]s in the page that do not have `.hasPicker` yet.
-    const dateInputs = document.querySelectorAll(`input[type="date"]:not(.hasPicker)`);
-    [].forEach.call(dateInputs, dateInput=> {
-      // Call Picker function on the input.
-      new Picker(dateInput);
-      // Mark that it have picker.
-      dateInput.classList.add(`hasPicker`);
-    });
+    // Get and loop all the input[type="date"]s in the page that do not have `[data-has-picker]` yet.
+    const dateInputs = document.querySelectorAll(`input[type="date"]:not([data-has-picker])`);
+
+    for(let input of dateInputs) {
+      new Input(input);
+    }
   }
 }
 
 // Run the above code on any <input type="date"> in the document, also on dynamically created ones.
 // Check if type="date" is supported.
-if(!Picker.checkDateInputSupport()) {
-  Picker.addPickerToDateInputs();
+if(!Input.supportsDateInput()) {
+  document.addEventListener(`DOMContentLoaded`, ()=> {
+    Input.addPickerToDateInputs();
+  });
   // This is also on mousedown event so it will capture new inputs that might
   // be added to the DOM dynamically.
-  document.querySelector(`body`).addEventListener(`mousedown`, e=> {
-    Picker.addPickerToDateInputs();
+  document.querySelector(`body`).addEventListener(`mousedown`, ()=> {
+    Input.addPickerToDateInputs();
   });
 }
