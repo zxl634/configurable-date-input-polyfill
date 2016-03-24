72b3452b226d5d6fb5874aea99ef842c84b5ced3
Brian Blakely <anewpage.media@gmail.com>
2016-03-24 04:25:06 +0000

    Codebase is split into granular modules. Upcoming: official localization support.

---
+++ locales.js
@@ -0,0 +1,58 @@
+// Localizations for UI text.
+// Keys correspond to applicable `lang` values, delimited by an underscore.
+// Days and months must be listed in the order they should display.
+
+const locales = {
+  'en_en-US_en-UK': {
+    days: [
+      `Sun`,
+      `Mon`,
+      `Tue`,
+      `Wed`,
+      `Thu`,
+      `Fri`,
+      `Sat`
+    ],
+    months: [
+      `January`,
+      `February`,
+      `March`,
+      `April`,
+      `May`,
+      `June`,
+      `July`,
+      `August`,
+      `September`,
+      `October`,
+      `November`,
+      `December`
+    ]
+  },
+  'test_test-TEST': {
+    days: [
+      `Foo`,
+      `Mon`,
+      `Tue`,
+      `Wed`,
+      `Thu`,
+      `Fri`,
+      `Sat`
+    ],
+    months: [
+      `Foo`,
+      `February`,
+      `March`,
+      `April`,
+      `May`,
+      `June`,
+      `July`,
+      `August`,
+      `September`,
+      `October`,
+      `November`,
+      `December`
+    ]
+  }
+};
+
+export default locales;
