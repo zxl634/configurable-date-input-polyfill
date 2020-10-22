5ddd555c8f3d0d1383dc959ef2fa727192f83092
KreutzerCode <KreutzerCode@gmail.com>
2020-10-22 22:50:17 +0200

    [TASK] Migrates Webpack configuration from v4 to v5

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,25 @@
 const path = require('path');
 
 module.exports = {
-    entry: {
-        app: './configurable-date-input-polyfill.js'
-    },
+    entry: './configurable-date-input-polyfill.js',
     output: {
         filename: 'configurable-date-input-polyfill.dist.js',
-        path: path.resolve(__dirname, '')
+        path: path.resolve(__dirname, ''),
+        environment: {
+            arrowFunction: false, // The environment supports arrow functions.
+            bigIntLiteral: false, // The environment supports BigInt as literal.
+            destructuring: false, // The environment supports destructuring.
+            dynamicImport: false, // The environment supports an async import() function to import EcmaScript modules.
+            module: false, // The environment supports ECMAScript Module syntax to import ECMAScript modules.
+            const: false, // The environment supports const and let for variable declarations.
+            forOf: false, // The environment supports 'for of' iteration.
+        },
     },
-    mode: 'development',
     module: {
         rules: [
             {
-                test: /\.js$/, // include .js files
-                exclude: path.resolve(__dirname, "node_modules"), // exclude any and all files in the node_modules folder
+                test: /\.m?js$/,
+                exclude: /node_modules/,
                 use: [{
                     loader: "babel-loader",
                     options: {
@@ -23,7 +29,7 @@ module.exports = {
             },
             {
                 test: /\.(s*)css$/,
-                exclude: path.resolve(__dirname, "node_modules"), // exclude any and all files in the node_modules folder
+                exclude: /node_modules/,
                 use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
